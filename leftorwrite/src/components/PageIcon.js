65c283dca0d3850494e96f915b01461e089d7883
Theresa Benkman <konatess@gmail.com>
2022-06-02 14:27:01 -0700

    Changed colors for page icons / connection buttons, and added drop shadows.

--- leftorwrite/src/components/PageIcon.js
+++ leftorwrite/src/components/PageIcon.js
@@ -5,13 +5,13 @@ import icons from '../constants/icons';
 export default function PageIcon (props) {
     return <a className={ "pageIcon" + (props.page.contentFinished ? "" : " unfinPageIcon")} href={strings.fRoutes.writep.replace(":pageid", 6)}>
             <div className='parentStatus'>
-                {props.page.isOrphaned && icons.link({className: "pageStatus pLinkIcon", id: "parentlink" + props.page.id})}
+                {props.page.isOrphaned && icons.parentlink({className: "pageStatus pLinkIcon", id: "parentlink" + props.page.id})}
                 {props.page.isStart && icons.start({className: "pageStatus pStartIcon", id: "start" + props.page.id})}
             </div>
         <div className='childStatus'>
             {props.page.isTBC && icons.tbc({className: "pageStatus pTBCIcon", id: "tbc" + props.page.id})}
             {props.page.isEnding && icons.end({className: "pageStatus pEndIcon", id: "ending" + props.page.id})}
-            {!props.page.isLinked && icons.link({className: "pageStatus pLinkIcon", id: "childlink" + props.page.id})}
+            {!props.page.isLinked && icons.childlink({className: "pageStatus pLinkIcon", id: "childlink" + props.page.id})}
         </div>
     </a>
 }
--- leftorwrite/src/constants/icons.js
+++ leftorwrite/src/constants/icons.js
@@ -1,26 +1,33 @@
-import { FaFeatherAlt, FaShare, FaCircle, FaBahai, FaLink, FaSquare, FaForward, FaClone, FaPlay } from "react-icons/fa";
+import { FaCircle, FaBahai, FaSquare, FaLink, FaPlay, FaForward, FaBookmark,  
+    FaRegBookmark, FaFeatherAlt, FaHeart, FaRegHeart, FaTimes, FaQuestion, FaFile,
+    FaRegFile, FaFilter, FaShare, FaGhost, FaHandsHelping, FaBell,
+    FaAngleUp, FaAngleDown,
+    // FaClone, FaShareAltSquare, FaProjectDiagram, FaShapes, FaArrowsAlt, FaArrowsAltH 
+} from "react-icons/fa";
 
 export default {
     start: (props) => { return <FaCircle className={props.className} id={props.id}/> },
     tbc: (props) => { return <FaBahai className={props.className} id={props.id}/> },
     end: (props) => { return <FaSquare className={props.className} id={props.id}/> },
     link: (props) => { return <FaLink className={props.className} id={props.id}/> }, // FaAngleUp FaAngleDown FaCaretDown FaCaretUp FaChevronUp FaChevronDown FaExclamationCircle FaBolt FaLink
+    parentlink: (props) => { return <FaAngleDown className={props.className} id={props.id}/> },
+    childlink: (props) => { return <FaAngleUp className={props.className} id={props.id}/> },
     continue: (props) => { return <FaPlay className={props.className} id={props.id}/> }, // FaPlay FaForward
     choice: (props) => { return <FaForward className={props.className} id={props.id}/> }, // FaProjectDiagram FaArrowsAlt FaArrowsAltH FaShapes FaShareAltSquare FaForward
-    bookmark: 'FaBookmark',
-    bookmarkline: 'FaRegBookmark',
+    bookmark: (props) => { return <FaBookmark className={props.className} id={props.id}/> },
+    bookmarkline: (props) => { return <FaRegBookmark className={props.className} id={props.id}/> },
     feather: (props) => { return <FaFeatherAlt className={props.className} id={props.id}/> },
-    heart: 'FaHeart',
-    heartline: 'FaRegHeart',
-    close: 'FaTimes',
-    question: 'FaQuestion',
-    page: 'FaFile',
-    pageline: 'FaRegFile',
-    filter: 'FaFilter',
+    heart: (props) => { return <FaHeart className={props.className} id={props.id}/> },
+    heartline: (props) => { return <FaRegHeart className={props.className} id={props.id}/> },
+    close: (props) => { return <FaTimes className={props.className} id={props.id}/> },
+    question: (props) => { return <FaQuestion className={props.className} id={props.id}/> },
+    page: (props) => { return <FaFile className={props.className} id={props.id}/> },
+    pageline: (props) => { return <FaRegFile className={props.className} id={props.id}/> },
+    filter: (props) => { return <FaFilter className={props.className} id={props.id}/> },
     curvedarrow: (props) => {return <FaShare className={props.className} id={props.id}/>}, // FaShareSquare
-    ghost: 'FaGhost',
-    cooperate: 'FaHandsHelping',
-    alert: 'FaBell'
+    ghost: (props) => { return <FaGhost className={props.className} id={props.id}/> },
+    cooperate: (props) => { return <FaHandsHelping className={props.className} id={props.id}/> },
+    alert: (props) => { return <FaBell className={props.className} id={props.id}/> }
 }
 
 // FaStar
